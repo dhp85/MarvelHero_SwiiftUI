1c0d96a71367c31a6ce7db11f202b63307ae8296
Diego Herreros <diegohp85@gmail.com>
2024-12-17 15:42:59 +0100

    Initial Commit

---
+++ MarvelHero_SwiiftUI/MarvelHero_SwiiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelHero_SwiiftUIApp.swift
+//  MarvelHero_SwiiftUI
+//
+//  Created by Diego Herreros Parron on 17/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MarvelHero_SwiiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
