c70d9534bb2afc4c1abe9227dcb1458789eb04a7
Diego Herreros <diegohp85@gmail.com>
2024-12-21 18:32:22 +0100

    Feat: Refactoring constants API_KEY y HASH

--- MarvelHero_SwiiftUI/Domain/Tools/ConstantApp.swift
+++ MarvelHero_SwiiftUI/Domain/Tools/ConstantApp.swift
@@ -10,6 +10,6 @@ import Foundation
 struct ConstantApp {
     static let CONSTANT_API_URL: String = "https://gateway.marvel.com//v1/public/"
     static let CONSTANT_API_URL_SERIES: String = "https://gateway.marvel.com//v1/public/characters/"
-    static let API_KEY: String = "3a1dae965b132b86c92b22da6ae7ba5b"
-    static let HASH: String = "40f1d779544ac65d98a4233fde47a675"
+    static let API_KEY: String = "" //Pegar aqui public key.
+    static let HASH: String = "" //Pegar aqui hash.
 }
